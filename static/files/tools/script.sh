7cea4ce91bed408d8a1dcf5013448887a5f11506
Marie-Helene Burle <marie.burle@westgrid.ca>
2022-03-08 12:50:58 -0800

    script: add timing for rg recursive

--- static/files/tools/script.sh
+++ static/files/tools/script.sh
@@ -13,3 +13,9 @@ less /home/marie/parvus/prog/slides/static/files/tools/two_layer_net_tensor.py
 bat /home/marie/parvus/prog/slides/static/files/tools/julia.jl
 
 less /home/marie/parvus/prog/slides/static/files/tools/julia.jl
+
+##############################################################################
+
+hyperfine -i --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_caches' 'grep -r Unix ~/parvus/prog/tcl'
+
+hyperfine --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_caches' 'rg Unix ~/parvus/prog/tcl'
