a843b71c849ef1059a0287025f5121b90f413ba0
Marie-Helene Burle <marie.burle@westgrid.ca>
2022-03-08 12:50:35 -0800

    script for commands to run in tools webinar: add commands for bat

---
+++ static/files/tools/script.sh
@@ -0,0 +1,15 @@
+cat /home/marie/parvus/prog/slides/static/files/tools/julia.jl
+
+bat /home/marie/parvus/prog/slides/static/files/tools/julia.jl
+
+##############################################################################
+
+bat /home/marie/parvus/prog/slides/static/files/tools/two_layer_net_tensor.py
+
+cat /home/marie/parvus/prog/slides/static/files/tools/two_layer_net_tensor.py
+
+less /home/marie/parvus/prog/slides/static/files/tools/two_layer_net_tensor.py
+
+bat /home/marie/parvus/prog/slides/static/files/tools/julia.jl
+
+less /home/marie/parvus/prog/slides/static/files/tools/julia.jl
