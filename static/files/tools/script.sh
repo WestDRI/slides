51724d59cbf910c3919b2b2acdc68ba7fedf8a44
Marie-Helene Burle <marie.burle@westgrid.ca>
2022-03-15 15:42:16 -0700

    script: add commands for pass

--- static/files/tools/script.sh
+++ static/files/tools/script.sh
@@ -27,5 +27,32 @@ hyperfine -i --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_caches' 'grep 
 hyperfine --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_caches' 'rg Unix ~/parvus/prog/tcl'
 # pass ##############################################################################
 
+bat /usr/bin/pass
+
+pass
+
+pass init mhburle@gmail.com
+
+pass
+
+pass insert somesite.ca/your@email
+
+pass
+
+pass somesite.ca/your@email
+pass -c somesite.ca/your@email
+
+pass edit somesite.ca/your@email
+
+pass generate -c othersite.com/userid 12
+
+pass
+
+pass generate -cn newsite.ca/your@email 20
+
+pass rm newsite.ca/your@email
+
+pass
+
 # tramp ##############################################################################
 # helm ##############################################################################
