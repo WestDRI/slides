a2ddaf2a973ad079bef0186d0bec55283bd57760
Marie-Helene Burle <marie.burle@westgrid.ca>
2022-03-17 09:20:57 -0700

    script: shorten lines to have better double screen setup

--- static/files/tools/script.sh
+++ static/files/tools/script.sh
@@ -1,6 +1,6 @@
-# lazygit ##############################################################################
+# lazygit #################################################################
 # go to repo /home/marie/parvus/ptmp/git_workshop_collab
-# bat #############################################################################
+# bat #####################################################################
 cat /home/marie/parvus/prog/slides/static/files/tools/julia.jl
 bat /home/marie/parvus/prog/slides/static/files/tools/julia.jl
 #################
@@ -19,10 +19,10 @@ bat -p /home/marie/parvus/ptmp/git_workshop_collab/src/test_manuel.py
 bat -pp /home/marie/parvus/ptmp/git_workshop_collab/src/test_manuel.py
 bat -pp /home/marie/parvus/ptmp/git_workshop_collab/src/test_manuel.py | more
 bat -fpp /home/marie/parvus/ptmp/git_workshop_collab/src/test_manuel.py | more
-# rg #############################################################################
 hyperfine -i --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_caches' 'grep -r Unix ~/parvus/prog/tcl'
 hyperfine --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_caches' 'rg Unix ~/parvus/prog/tcl'
-# pass ##############################################################################
+# rg #######################################################################
+# pass ######################################################################
 bat /usr/bin/pass
 pass
 pass init mhburle@gmail.com
@@ -37,10 +37,10 @@ pass
 pass generate -cn newsite.ca/your@email 20
 pass rm newsite.ca/your@email
 pass
-# tramp ##############################################################################
+# tramp ####################################################################
 emacsclient -c /ssh:mhburle@cedar.computecanada.ca:/home/mhburle/.bash_history
 emacsclient -c /sudo::/etc/ssh/sshd_config
-# helm ##############################################################################
+# helm ######################################################################
 # open .emacs and show:
 # ; j
 # ; i
